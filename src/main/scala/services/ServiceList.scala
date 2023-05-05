cc3f6efdbea3aaecdf35f667dacd5f22ef053378
Oleg <dodigrams@gmail.com>
2023-05-05 18:46:38 +0300

    правки

--- src/main/scala/services/ServiceList.scala
+++ src/main/scala/services/ServiceList.scala
@@ -3,7 +3,6 @@ package services
 import cats.Applicative
 import cats.effect.{IO, Sync}
 import io.circe.*
-import io.circe.generic.semiauto.deriveDecoder
 import io.circe.literal.*
 import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
 import org.http4s.circe.jsonOf
@@ -22,8 +21,10 @@ object ServiceList {
       val passwordRepeat = json.hcursor.get[String]("passwordRepeat").toOption.getOrElse("")
       val email = json.hcursor.get[String]("email").toOption.getOrElse("")
       val age = json.hcursor.get[Int]("age").toOption.getOrElse(0)
+
       val photoBytes = json.hcursor.get[Array[Byte]]("photo").getOrElse(Array.emptyByteArray)
       val photoPath = PhotoService.uploadPhoto(photoBytes)
+
       val user = User(login, password, passwordRepeat, Some(age), email, photoPath)
 
       val validationInfo = validation.checkValidation(user)
