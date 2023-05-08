b8a22a14e0ab4fe92fee20702e55060c48392ef0
Oleg <dodigrams@gmail.com>
2023-05-08 22:38:28 +0300

    проверка на формат

--- src/main/scala/services/PhotoService.scala
+++ src/main/scala/services/PhotoService.scala
@@ -15,9 +15,21 @@ object PhotoService {
 
     Files.createDirectories(avatarPath)
 
-    val outputStream = new FileOutputStream(new File( s"$avatarPath\\$login.jpg"))
+    val inputStream = new ByteArrayInputStream(photoByte)
+    val imageFormat = ImageIO.read(inputStream).getType match
+      case BufferedImage.TYPE_INT_RGB | BufferedImage.TYPE_INT_ARGB =>
+        if (ImageIO.getUseCache) "jpg" else "jpeg"
+      case BufferedImage.TYPE_INT_BGR =>
+        if (ImageIO.getUseCache) "bjpg" else "jpeg"
+      case BufferedImage.TYPE_3BYTE_BGR => "jpg"
+      case BufferedImage.TYPE_4BYTE_ABGR => "png"
+      case BufferedImage.TYPE_BYTE_GRAY | BufferedImage.TYPE_USHORT_GRAY => "png"
+      case BufferedImage.TYPE_BYTE_BINARY => "webp"
+      case _ => "unknown"
+
+    val outputStream = new FileOutputStream(new File( s"$avatarPath\\$login.$imageFormat"))
 
     outputStream.write(photoByte)
     outputStream.close()
-    s"$avatarPath\\$login.jpg"
+    s"$avatarPath\\$login.$imageFormat"
 }
